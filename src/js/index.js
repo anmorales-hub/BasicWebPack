828dda7fedb74533407044d21ccc0f0c86f742bd
anmorales-hub <moralesand2018@gmail.com>
2021-04-23 19:14:33 -0400

    Task: Fix Table Generation

--- src/js/index.js
+++ src/js/index.js
@@ -1,3 +1,25 @@
-import TestJS from './TestJs.js';
+import TestJS from "./TestJs.js";
+import ConsoleLogIt from "./ConsoleLogIt.js";
+import getJSON from "./getJSON";
+const MakeTable = require('./makeTable');
 
 TestJS();
+getJSON("", function (data) {
+    console.log(data);
+});
+
+getJSON('http://localhost:8080/api/v1/cities',
+    function(err, records) {
+        if (err !== null) {
+            alert('Something went wrong: ' + err);
+        } else {
+            let table = document.querySelector("table");
+            let data = Object.keys((records.data[0]));
+            let dataRecords = records.data;
+
+            MakeTable.makeHeader(table, data);
+            MakeTable.makeTable(table, dataRecords);
+        }
+
+    });
+ConsoleLogIt("Everthing is generated");
